1c8dbd6bf5dd72ec7f80b5e826995e3dbcd54009
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2025-01-19 12:19:02 -0300

    refatora o ChatBot para integrar o LangChain e ChromaDB, adiciona inicialização do banco de dados vetorial e tratamento de erros

--- ChatBot.py
+++ ChatBot.py
@@ -1,45 +1,58 @@
-import chromadb
-from ollama import chat
-from ollama import ChatResponse
-from chromadb.config import Settings
-from DataBase import loadDocument, storageInChroma
+from langchain_ollama.llms import OllamaLLM
+from DataBase import loadDocument, storageInChroma, loadAndStoreDocument, initializeChromaDB
+
+vectorstore = initializeChromaDB()
+
+question = "quais documentos eu preciso para comprovar a renda?"
+retrive = vectorstore.as_retriever(
+    search_type = "similarity",
+    search_kwargs = {"k":3}
+)
+
+retrive_doc = retrive.invoke(question)
+
+context = ' '.join([doc.page_content for doc in retrive_doc])
+print(context)
+
+llm = OllamaLLM(model = "llama3.2:1b")
 
 
 
+response = llm.invoke(f"""Responda a pergunta de acordo com o contexto dado previamente: Pergunta {question} Contexto: {context}""")
+
+print(response)
 
 chat_history = []
 
-def initialize_model(model="all-MiniLM-L6-v2"):
-    return model
+
+
+
+
+
+
+
+# print("\n============iniciando================")
+# print("ChatBot -> Olá, sou um ChatBot e estou aqui para te ajudar. Digite 'exit' para sair.\n")
+
+# while True:
+#     question = input("Você -> ")
+#     if question == "exit":
+#         break
     
-model = initialize_model()
-
-
-
-
-
-print("\n============iniciando================")
-print("ChatBot -> Olá, sou um ChatBot e estou aqui para te ajudar. Digite 'exit' para sair.\n")
-
-while True:
-    question = input("Você -> ")
-    if question == "exit":
-        break
+#     # Busca no ChromaDB
+#     similar_response = query_chromadb(question, model, collection)
     
-    # Busca no ChromaDB
-    similar_response = query_chromadb(question, model, collection)
+#     # Adiciona ao histórico do chat
+#     chat_history.append({'role': 'user', 'content': question})
+#     chat_history.append({'role': 'assistant', 'content': f"FAQ -> {similar_response}"})
     
-    # Adiciona ao histórico do chat
-    chat_history.append({'role': 'user', 'content': question})
-    chat_history.append({'role': 'assistant', 'content': f"FAQ -> {similar_response}"})
+#     # Gera resposta usando Ollama
+#     response = chat(model='llama3.2:1b', messages=chat_history + [
+#         {
+#             'role': 'user',
+#             'content': 'Faça uma resposta clara e objetiva usando de base o texto fornecido.',
+#         },
+#     ], options={"Temperature": 0.2})
     
-    # Gera resposta usando Ollama
-    response = chat(model='llama3.2:1b', messages=chat_history + [
-        {
-            'role': 'user',
-            'content': 'Faça uma resposta clara e objetiva usando de base o texto fornecido.',
-        },
-    ], options={"Temperature": 0.2})
-    
-    final_response = response.message.content
-    print(f"ChatBot -> {final_response}\n")
+#     final_response = response.message.content
+#     print(f"ChatBot -> {final_response}\n")
--- DataBase.py
+++ DataBase.py
@@ -5,11 +5,28 @@ from langchain_community.vectorstores import Chroma
 
 default_persist_directory = "./chroma_langchain_db"
 
-vector_store = Chroma(
-    collection_name="Editais_UFPA",
-    embedding_function=OllamaEmbeddings(model="all-minilm:33m"),
-    persist_directory=default_persist_directory,  # Where to save data locally, remove if not necessary
-)
+def initializeChromaDB(persist_directory=default_persist_directory):
+    """
+    Inicializa um banco de dados vetorial com persistência usando ChromaDB.
+    
+    Args:
+        persist_directory (str): Diretório para persistir os dados do ChromaDB.
+    
+    Returns:
+        Chroma: O banco de dados vetorial inicializado.
+    """
+    try:
+        vector_store = Chroma(
+            collection_name="Editais_UFPA",
+            embedding_function=OllamaEmbeddings(model="all-minilm:33m"),
+            persist_directory=persist_directory,  # Where to save data locally, remove if not necessary
+        )
+        print(f"Banco de dados vetorial inicializado com sucesso!")
+        return vector_store
+    except Exception as e:
+        print(f"Erro ao inicializar o banco de dados vetorial: {e}")
+        return None
+
 
 def loadDocument(pdf_path):
     """
@@ -37,7 +54,7 @@ def loadDocument(pdf_path):
         return None
 
 
-def storageInChroma(documents, persist_directory=default_persist_directory, vector_db=vector_store):
+def storageInChroma(documents, persist_directory=default_persist_directory, vector_db=initializeChromaDB()):
     """
     Armazena os documentos em um banco de dados vetorial usando ChromaDB com persistência.
     
@@ -54,7 +71,7 @@ def storageInChroma(documents, persist_directory=default_persist_directory, vect
         vector_db.add_documents(documents=documents)
         
         print(f"Documentos armazenados no diretório: {persist_directory}")
-        return vector_store
+        return vector_db
     except Exception as e:
         print(f"Erro ao armazenar os documentos no ChromaDB: {e}")
         return None
@@ -100,44 +117,9 @@ def loadAndStoreDocument(persist_directory=default_persist_directory):
         return None
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    # Processar e armazenar o documento
-    #vectorstore = loadAndStoreDocument()
-    print(vector_store.similarity_search("caso eu não tenha familiar com conta bancária, o que eu devo fazer?", k=3))
-    
-    
-
-
-
-# '''loader = PyPDFLoader(r"C:\Users\Christian\OneDrive - Universidade Federal do Pará - UFPA\TRABALHOS\8 Periodo\Engenharia de Software\Chatbot-FAQ-Whatsapp\Editais\Edital_05_2024_COPERPS_PS2025_retificado_v3.pdf")
-
-# doc = loader.load()
-
-# text_splitter = RecursiveCharacterTextSplitter(
-#     chunk_size= 1200,
-#     chunk_overlap = 100,
-#     add_start_index = True
-# )
-
-# all_splitts = text_splitter.split_documents(doc)
-
-# loocal_embeddings = OllamaEmbeddings(model="all-minilm:33m")
-
-# vectorstore = Chroma.from_documents(documents=all_splitts, embedding=loocal_embeddings)'''
-
-# '''question = "caso eu não tenha familiar com conta bancária, o que eu devo fazer?"
-# retrive = vectorstore.as_retriever(
-#     search_type = "similarity",
-#     search_kwargs = {"k":3}
-# )
-
-# retrive_doc = retrive.invoke(question)
-
-# context = ' '.join([doc.page_content for doc in retrive_doc])
-
-# llm = OllamaLLM(model = "llama3.2:1b")
-
-# response = llm.invoke(f"""Responda a pergunta de acordo com o contexto dado previamente: Pergunta {question} Contexto: {context}""")
-
-# print(response)'''
+#     #Processar e armazenar o documento
+#     #vectorstore = loadAndStoreDocument()
+#     vector_store = initializeChromaDB()
+#     print(vector_store.similarity_search("caso eu não tenha familiar com conta bancária, o que eu devo fazer?", k=3))
